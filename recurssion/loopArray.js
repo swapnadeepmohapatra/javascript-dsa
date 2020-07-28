1e73c24bc55c7da72f11c0a6d6bb9ca82d2da92d
Swapnadeep Mohapatra <41564532+swapnadeepmohapatra@users.noreply.github.com>
2020-07-28 18:23:17 +0530

    Added looping in array with Recursion

--- recurssion/loopArray.js
+++ recurssion/loopArray.js
@@ -4,12 +4,7 @@ function iterate(arr, start = 0) {
     return;
   }
 
+  console.log(arr[start]);
   iterate(arr, start + 1);
-  //   return arr;
-  //   console.log(arr[start]);
 }
-console.log(iterate([1, 2, 3, 4, 5, 6, 7, 8, 9]));
-// iterate([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-// iterate([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-// 3 + 6 + 9 = 18
-// console.log(sum);
+iterate([1, 2, 3, 4, 5, 6, 7, 8, 9]);
